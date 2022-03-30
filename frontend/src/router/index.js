93ab3bb63f994d50f0c424277cf31b29228a52fb
krummja <crumja@uga.edu>
2022-03-30 19:41:43 -0400

    Wed 30 Mar 2022 07:41:43 PM EDT

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -7,6 +7,11 @@ const routes = [
         path: '/',
         name: 'home',
         component: Home
+    },
+    {
+        path: '/about',
+        name: 'about',
+        component: () => import('../views/About.vue')
     }
 ];
 
