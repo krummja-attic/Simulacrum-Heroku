4a70b243a28b84c0c28e61c5175f8f62fbbc20c6
krummja <crumja@uga.edu>
2022-03-30 15:28:05 -0400

    Updating tables

--- backend/data.sql
+++ backend/data.sql
@@ -1,2 +1,2 @@
-insert into user values(1, 'admin', 'admin');
-insert into post values(1, 'default', '2022-03-30 15:09:00.000000', 'default', 1);
+INSERT INTO user VALUES(1, 'admin', 'admin');
+INSERT INTO post VALUES(1, 'default', '2022-03-30 15:09:00.000000', 'default', 1);
