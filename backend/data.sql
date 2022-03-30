a59cb8aa89ad4a56863528ffa34d6d796422eab2
krummja <crumja@uga.edu>
2022-03-30 16:49:51 -0400

    Wed 30 Mar 2022 04:49:51 PM EDT

--- backend/data.sql
+++ backend/data.sql
@@ -1,2 +1,1 @@
-INSERT INTO user VALUES(1, 'admin', 'admin');
-INSERT INTO post VALUES(1, 'default', '2022-03-30 15:09:00.000000', 'default', 1);
+INSERT INTO public.post VALUES(1, 'default', '2022-03-30 15:09:00.000000', 'default', 1);
