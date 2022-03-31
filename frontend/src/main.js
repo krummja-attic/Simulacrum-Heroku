d3cde53d2d58d98feb538b6166d7038bc514533f
krummja <crumja@uga.edu>
2022-03-30 20:47:18 -0400

    Wed 30 Mar 2022 08:47:18 PM EDT

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,9 @@
-import Vue from 'vue';
-// import { createApp } from 'vue';
+
+import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 
 // const app = createApp(App);
-const app = Vue.createApp(App);
+const app = createApp(App);
 app.use(router);
 app.mount('#app');
