ff5c9e982c135770b58520bd1333e9cac94b3a79
Jonathan Crum <crumja@uga.edu>
2022-06-17 08:48:07 -0400

    06/17/2022 08:48:07

--- server/dispatcher.py
+++ server/dispatcher.py
@@ -14,3 +14,8 @@ frontend = SharedDataMiddleware(NotFound(), {
 app = DispatcherMiddleware(frontend, {
     '/api': create_app()
 })
+
+
+if __name__ == '__main__':
+    from werkzeug.serving import run_simple
+    run_simple('127.0.0.1', 5000, app, use_debugger=True, use_reloader=True)
